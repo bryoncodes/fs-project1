231107de308e609fb8a81c848d088b383421efdc
Bryon <bryonb@gmail.com>
2015-08-19 21:35:25 -0700

    Stylistic changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fs-project1
-Run "python media.py" or ensure executable permissions are set and run "./media.py"
+This project generates a webpage that showcases some of my favorite movies.
+
+Run "python media.py" or ensure executable permissions are set and run "./media.py" then open fresh_tomatoes.html in your browser of choice.
+
+Made with Python 2.7, your mileage may vary with differing versions. 
--- media.py
+++ media.py
@@ -3,13 +3,14 @@
 # import the provided module
 import fresh_tomatoes
 
-# define movie class
-# constructor takes the following parameters:
-#   string with movie title
-#   string with url to poster art
-#   string with url to trailer on YouTube
-#   list of strings of actors
 class movie:
+  """
+  movie class, constructor takes the following parameters:
+    string with movie title
+    string with url to poster art
+    string with url to trailer on YouTube
+    list of strings of actors
+  """
 
     def __init__ (self,title, art_url, trailer_url, actors):
         self.trailer_youtube_url = trailer_url
@@ -17,12 +18,36 @@ class movie:
         self.title = title
         self.actors = actors
 
-#instantiate some movie objects
-m1 = movie("Idiocracy", "http://i.imgur.com/UWHnbYv.jpg", "https://www.youtube.com/watch?v=BBvIweCIgwk", ["Luke Wilson", "Maya Rudolph", "Justin Long"])
-m2 = movie("Full Metal Jacket", "http://i.imgur.com/k67OMUe.jpg", "https://www.youtube.com/watch?v=x9f6JaaX7Wg", ["R. Lee Ermy", "Matthew Modine", "Adam Baldwin"] )
-m3 = movie("Office Space", "http://i.imgur.com/952MNeQ.jpg", "https://www.youtube.com/watch?v=_IwzZYRejZQ", ["Ron Livingstone", "Jennifer Aniston", "Gary Cole"] )
-m4 = movie("Sling Blade", "http://i.imgur.com/K5OESky.jpg", "https://www.youtube.com/watch?v=4sQuu36ZKQg", ["Billy Bob Thornton", "Dwight Yoakum", "John Ritter"] )
-m5 = movie("Blazing Saddles", "http://i.imgur.com/JP0vley.jpg", "https://www.youtube.com/watch?v=iLNQv19YpG4", ["Cleavon Little", "Gene Wilder", "Madeline Kahn"] )
+# instantiate some movie objects
+m1 = movie(
+  "Idiocracy",
+  "http://i.imgur.com/UWHnbYv.jpg",
+  "https://www.youtube.com/watch?v=BBvIweCIgwk",
+  ["Luke Wilson", "Maya Rudolph", "Justin Long"])
+
+m2 = movie(
+  "Full Metal Jacket",
+  "http://i.imgur.com/k67OMUe.jpg",
+  "https://www.youtube.com/watch?v=x9f6JaaX7Wg",
+  ["R. Lee Ermy", "Matthew Modine", "Adam Baldwin"])
+
+m3 = movie(
+  "Office Space",
+  "http://i.imgur.com/952MNeQ.jpg",
+  "https://www.youtube.com/watch?v=_IwzZYRejZQ",
+  ["Ron Livingstone", "Jennifer Aniston", "Gary Cole"])
+
+m4 = movie(
+  "Sling Blade",
+  "http://i.imgur.com/K5OESky.jpg",
+  "https://www.youtube.com/watch?v=4sQuu36ZKQg",
+  ["Billy Bob Thornton", "Dwight Yoakum", "John Ritter"])
+
+m5 = movie(
+  "Blazing Saddles",
+  "http://i.imgur.com/JP0vley.jpg",
+  "https://www.youtube.com/watch?v=iLNQv19YpG4",
+  ["Cleavon Little", "Gene Wilder", "Madeline Kahn"])
 
 # put instantiated objects in a list
 movies = [m1, m2, m3, m4, m5]
